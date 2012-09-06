f4c099b836f48bd2829efb5e0a7195c62034aff1
TehMushy <root@tehmushy.info>
2012-09-06 20:47:47 +0100

    index.php starting code added

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>mWoW > home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<style>
+		body {
+			padding-top: 60px;
+		}
+	</style>
+  </head>
+  <body>
+	<div class="navbar navbar-fixed-top">
+		<a class="brand" href="#">mWoW - A simple WoW Dungeon/raid DB</a>
+	</div>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+			<div class="well">
+				<ul class="nav nav-list">
+					<li class="nav-header">Menu</li>
+						<li class="active"><a href"#">Home</a></li>
+						<li><a href="#">About</a>
+					<li class="nav-header">Classic WoW</li>
+						<li><a href="#">Dungeons</a>
+						<li><a href="#">Raids</a>
+				</ul>
+			</div>
+			</div>
+				<div class="span10">
+					<div class="page-header">
+						<h1>title plateholer <small>awwye.png</small></h1>
+					</div>
+			</div>
+		</div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
