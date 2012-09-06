24124d2e0008bdfd5ba20f63f62be0b6fc0240e8
TehMushy <root@tehmushy.info>
2012-09-07 00:34:19 +0100

    cleaned up index navbar, added gpage and finished navmenu

---
+++ gpage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>mWoW > gPage</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/gheart.css" rel="stylesheet">
+	<style>
+		body {
+			padding-top: 60px;
+		}
+	</style>
+  </head>
+  <body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="row">
+				<div class="span6 offset10">
+					<a class="brand" href="#">mWoW - A simple WoW Dungeon/raid DB</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span2">
+			<div class="well">
+				<ul class="nav nav-list">
+						<li><a href="index.php">Home</a>
+						<li class="active"><a href="#">Guild Page</a></li>
+					<li class="divider"></li>
+					<li class="nav-header">Links</li>
+						<li><a href="http://eu.battle.net/wow/en/guild/draenor/SausageFest/">Summary</a>
+						<li><a href="http://eu.battle.net/wow/en/guild/draenor/SausageFest/roster">Roster</a>
+						<li><a href="http://eu.battle.net/wow/en/guild/draenor/SausageFest/news">News</a>
+						<li><a href="http://eu.battle.net/wow/en/guild/draenor/SausageFest/achievements">Achievements</a>
+						<li><a href="http://eu.battle.net/wow/en/guild/draenor/SausageFest/perks">Perks</a>
+				</ul>
+			</div>
+			</div>
+				<div class="span10">
+					<div class="page-header">
+						<h1>SausageFest guild<small><i> best WoW guild in the ENTIRE UNIVERSE&trade;</i></small></h1>
+					</div>
+					<div class="container">
+						<div class="well-large guild-logo">&hearts;</div>
+					</div>
+				</div>
+		</div>
+	</div>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <title>mWoW > home</title>
     <link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
 	<style>
 		body {
 			padding-top: 60px;
@@ -11,31 +12,48 @@
   </head>
   <body>
 	<div class="navbar navbar-fixed-top">
+	<div class="container">
 		<div class="navbar-inner">
 			<div class="row">
-				<div class="span6 offset10">
-					<a class="brand" href="#">mWoW - A simple WoW Dungeon/raid DB</a>
+				<div class="span12">
+					<div class="centered">
+						<a class="brand" href="#">mWoW - A simple WoW Dungeon/raid DB</a>
+					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 	</div>
 	<div class="container-fluid">
 		<div class="row-fluid">
 			<div class="span2">
 			<div class="well">
 				<ul class="nav nav-list">
-					<li class="nav-header">Menu</li>
 						<li class="active"><a href"#">Home</a></li>
-						<li><a href="#">About</a>
-					<li class="nav-header">Classic WoW</li>
+						<li><a href="gpage.php">Guild Page</a>
+					<li class="divider"></li>
+					<li class="nav-header">Classic</li>
 						<li><a href="#">Dungeons</a>
 						<li><a href="#">Raids</a>
+					<li class="nav-header">Burning Crusiade</li>
+						<li><a href="#">Dungeons</a>
+						<li><a href="#">Raids</a>
+					<li class="nav-header">Wrath of the Lich King</li>
+						<li><a href="#">Dungeons</a>
+						<li><a href="#">Raids</a>
+					<li class="nav-header">Cataclysm</li>
+						<li><a href="#">Dungeons</a>
+						<li><a href="#">Raids</a>
+					<li class="nav-header">Mists of Pandas</li>
+						<li><a href="#">Dungeons</a>
+						<li><a href="#">Raids</a>
+						<li><a href="#">Scenarios</a>
 				</ul>
 			</div>
 			</div>
 				<div class="span10">
 					<div class="page-header">
-						<h1>title plateholer <small>awwye.png</small></h1>
+						<h1>mWoW, where win things happen<small><i> for a win game&trade;</i></small></h1>
 					</div>
 			</div>
 		</div>
